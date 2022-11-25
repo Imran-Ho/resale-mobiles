38efcb0e6ee6067fc3b8a95d81d0fdc7cf1ffa76
Imran-Ho <imranhossain36@gmail.com>
2022-11-25 14:40:35 +0600

    payment component added with orders

--- src/Dashboard/MyOrders/Payment/Payment.js
+++ src/Dashboard/MyOrders/Payment/Payment.js
@@ -1,9 +1,33 @@
+
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+import CheckoutForm from '../CheckoutForm/CheckoutForm';
+
+const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PK);
+console.log(stripePromise)
 
 const Payment = () => {
+
+    const bookings = useLoaderData()
+    console.log(bookings)
+    // const navigation = useNavigation();
+    const { title, resalePrice, } = bookings;
+
+    // if(navigation.state === "loading"){
+    //     return <Loading></Loading>
+    // }
     return (
-        <div>
-            <h2>payment</h2>
+        <div className='text-center w-auto my-10 border'>
+            <h2 className='text-3xl py-6'>Payment for {title}</h2>
+            <p>Please pay <strong>$ {resalePrice}</strong> for your appointment on {} at {}</p>
+            <h2 className='text-2xl text-blue-900 mt-6'>Provide Card information below.</h2>
+            <div className='w-auto mx-16 mb-12 '>
+                <Elements stripe={stripePromise}>
+                    <CheckoutForm bookings={bookings}/>
+                </Elements>
+            </div>
         </div>
     );
 };
